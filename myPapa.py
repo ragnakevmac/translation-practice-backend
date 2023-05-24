145d4c31c7d05e439b46787f4a928890b7d966ee
Kevin <ragnakevmac@gmail.com>
2023-05-24 23:03:42 +0900

    added another domain with a www alias

--- myPapa.py
+++ myPapa.py
@@ -24,7 +24,8 @@ openai.api_key = OPENAI_API_KEY
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://tensaihonyaku.com"}})
+CORS(app, resources={r"/*": {"origins": ["http://tensaihonyaku.com", "http://www.tensaihonyaku.com"]}})
+
 
 
 
